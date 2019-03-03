f65eb5abf0e2cec8a29c91a13e40335a8a8e8c8a
sferguson670 <sferguson670@gmail.com>
2019-03-03 14:10:30 -0700

    web server works
    got it working!!!!

--- hw2/server/src/main/scala/Main.scala
+++ hw2/server/src/main/scala/Main.scala
@@ -3,16 +3,44 @@ import java.io._
 import scala.io._
 
 object EchoServer {
-  def main(args: Array[String]) {
-	val server = new ServerSocket(9999)
-	while (true) {
-		val s = server.accept()
-		val in = new BufferedSource(s.getInputStream()).getLines()
-		val out = new PrintStream(s.getOutputStream())
+  def read_and_write(in: BufferedReader, out:BufferedWriter): Unit = {
+    println("Enter html name file")
+    val request = in.readLine()
+    val file = new File("./" + request + ".html")
+    if (!file.exists()) {
+      println("File not found")
+      out.write("HTTP 404")
+    }
+    else {
+      println("HTTP/1.1 200") //version & status code
+      println("Content-Type: text/plain") //type of data
+      println("Connection: close") //will close stream
+      println("\r\n") //end of headers
 
-		out.println(in.next())
-		out.flush()
-		s.close()
-		}
-	}
+      out.write(scala.io.Source.fromFile(request + ".html").mkString)
+    }
+    out.flush()
+    in.close()
+    out.close()
+  }
+
+  def serve(server: ServerSocket): Unit = {
+    val s = server.accept()
+    val in = new BufferedReader(new InputStreamReader(s.getInputStream))
+    val out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream))
+
+    read_and_write(in, out)
+
+    s.close()
+  }
+
+  def main(args: Array[String]) {
+    val server = new ServerSocket(9999)
+    println("Listening for connection on port 9999 ...")
+    while(true) {
+      serve(server)
+    }
+  }
 }
+
+//Used Oracle's Java "All about sockets" documentation
