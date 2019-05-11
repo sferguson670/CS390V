aa3ecd90b0819b1388f3387c216ffd8e2e75798e
sferguson670 <sferguson670@gmail.com>
2019-05-10 22:14:46 -0600

    added files & method
    method for reading file

--- hw5/EchoServer.ex
+++ hw5/EchoServer.ex
@@ -5,6 +5,7 @@ defmodule EchoServer do
     {:ok, socket} = :gen_tcp.listen(port,
       [:binary, packet: :line, active: false, reuseaddr: true])
     Logger.info "Accepting connections on port #{port}"
+	IO.puts "Enter name of html file"
     loop_acceptor(socket)
   end
 
@@ -15,7 +16,7 @@ defmodule EchoServer do
   end
 
   defp serve(socket) do
-    socket |> read_line() |> write_line(socket)
+    socket |> read_line() |>getFile() |> write_line(socket)
     :ok = :gen_tcp.close(socket)
   end
 
@@ -31,4 +32,19 @@ defmodule EchoServer do
   def main(args \\ []) do
     accept(9999)
   end
+  
+  defp getFile(name) do
+	  file = "#{name}.html"
+	  
+	  case File.open(file) do
+		  {:ok, file} -> "HTTP/1.1 200\nContent-Type: text/plain\nConnection: close\n" <> File.read(file)
+		  {:error, _} -> "File not found\nHTTP 404\n"
+	  end
+  end
+  @doc """
+  help provided from Myde Rojas,
+  https://codewords.recurse.com/issues/five/building-a-web-framework-from-scratch-in-elixir
+  https://elixir-lang.org/crash-course.html
+  https://elixir-lang.org/getting-started/io-and-the-file-system.html
+  """  
 end
